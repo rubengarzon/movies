096a9dfaeacccfca621c473c2289a4b7013197f4
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-19 12:59:49 +0200

    json peliculas creado

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import "./App.css";
 import { PageWrapper } from "./components/PageWrapper";
 import { Pelicula } from "./components/Pelicula";
+import { PeliculasJson } from "./peliculas.json";
 
 function App() {
+  let peliculas = PeliculasJson;
+
   return (
     <PageWrapper>
-      <Pelicula
-        titulo="oblivion (2012)"
-        calificacion="8.1"
-        director="Joss Whedon"
-        actores="Robert Downey Jr.,Chris Evans,Chris Hemsworth"
-        fecha="1 May 2015"
-        duracion="2h 21min"
-        img="images/uploads/mv1.jpg"
-      >
-        Earth's mightiest heroes must come together and learn to fight as a team
-        if they are to stop the mischievous Loki and his alien army from
-        enslaving humanity...
-      </Pelicula>
-
-      <Pelicula
-        titulo="into the wild (2014)"
-        calificacion="7.8"
-        director="Anthony Russo, Joe Russo"
-        actores="Chris Evans,
-                    Samuel L. Jackson,
-                    Scarlett Johansson"
-        fecha="1 May 2015"
-        duracion="2h 21min"
-        img="images/uploads/mv2.jpg"
-      >
-        As Steve Rogers struggles to embrace his role in the modern world, he
-        teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to
-        battle a new threat...
-      </Pelicula>
+      {peliculas.map((pelicula) => (
+        <Pelicula
+          titulo={pelicula.titulo}
+          calificacion={pelicula.calificacion}
+          director={pelicula.director}
+          actores={pelicula.actores}
+          fecha={pelicula.fecha}
+          duracion={pelicula.duracion}
+          img={pelicula.img}
+        >
+          {pelicula.descripcion}
+        </Pelicula>
+      ))}
     </PageWrapper>
   );
 }
